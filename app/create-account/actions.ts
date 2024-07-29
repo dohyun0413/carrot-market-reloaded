3b01d51878ad9c6151317eda954fcc62c674ccc3
Alvin <jjangalvin@google.com>
2024-07-29 16:12:11 +0900

    #6.2

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,11 +1,24 @@
 "use server"
 import {z} from "zod";
 
+function checkUsername2(username : string){
+    return !username.includes("potato")
+}
+
+const checkUsername = (username : string) => !username.includes("potato")
+const checkPasswords = ({password, confirm_password} : {password:string, confirm_password:string}) => password === confirm_password;
+
 const formSchema = z.object({
-    username: z.string().min(3).max(10),
+    username: z.string({
+        invalid_type_error: "Username must be a string!", // 초기설정 자체가 잘못된 경우 username : formData.get("username") : ok / username : 1 : error
+        required_error: "where is my Username???" // username : formData.get("username") : ok / usernameeee : formData.get("username") : error
+    }).min(3, "Way too Short!!").max(10, "That is too long!!").refine(checkUsername, "No potatoes allowed"),
     email: z.string().email(),
     password: z.string().min(10),
     confirm_password: z.string().min(10)
+}).refine(checkPasswords, {
+    message : "Both paswords should be the same!",
+    path : ["confirm_password"],
 });
 
 export async function createAccount(prevState:any, formData:FormData){
